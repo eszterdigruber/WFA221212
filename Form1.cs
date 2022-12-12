6e77eb5c373b721744ac6d0161525beb3fd43c92
digruber.eszter <digruber.eszter@28-18>
2022-12-12 14:39:23 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,10 @@
+namespace WFA221212
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SQLQuery1.sql
@@ -0,0 +1,12 @@
+﻿--CREATE DATABASE adatbazis;
+--USE adatbazis;
+
+CREATE TABLE emberek (
+  id INT PRIMARY KEY IDENTITY,
+  nev VARCHAR(255),
+  szul
+
+);
+
+--Jó munkát -by Milán :)
+--Szia Eszti :)
